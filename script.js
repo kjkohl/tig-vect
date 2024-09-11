33fd2d960ba614ea24e7aec12e5e8504e95f607f
kjkohl <kaydenjkohl@gmail.com>
2024-09-11 07:46:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const Potrace = require('potrace');
 
+function uploadFile () {
+  const fileInput = document.getElementById('fileUpload');
+  const resultDiv = document.getElementById('result');
+
+  if (fileInput.files.length === 0) {
+    resultDiv.innerHTML = "<p>Please upload a file first.</p>";
+    return;
+  }
+
+  const file = fileInput.files[0];
+  if (!file.type.startsWith('image/')) {
+    resultDiv.innerHTML = "<p>Only image files are supported.</p>"
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.onload = function (event) {
+    const img =  new Image();
+    img.src = event.target.result;
+
+    Potrace.trace(img.src, function(err, svg) {
+      if (err) {
+        resultDiv.innerHTML = "<p>Conversion failed.</p>";
+        return;
+      }
+
+      resultDiv.innerHTML = `<div>${svg}</div>`;
+    });
+  };
+    reader.readAsDataURL(file);
+}
